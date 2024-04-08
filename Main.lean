8976c9f54207fab5538441f30f2df104909a2673
dxo <hi@d-xo.org>
2024-04-08 13:42:08 +0200

    do smth in main

--- Main.lean
+++ Main.lean
@@ -1,3 +1,11 @@
 import «SolcLean4»
 
-def main : IO Unit := IO.println "Hello"
+open Solidity
+open Solidity.Add
+
+def main : IO Unit := do
+
+  let res := runSol $ do
+    add (U256.abs 9) (U256.abs 12)
+
+  IO.println $ "result is: " ++ repr res
